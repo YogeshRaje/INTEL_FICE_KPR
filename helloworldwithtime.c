c41499d2c8ab1c7dd2ee952b8adca40081c2e889
Yogesh Raje <yhraje@gmail.com>
2022-11-15 15:32:39 +0530

    Update helloworldwithtime.c

--- helloworldwithtime.c
+++ helloworldwithtime.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <time.h>
-double time_spent = 0.0;
- 
-clock_t begin = clock();
+
 int main() {
 // printf() displays the string inside quotation
+	double time_spent = 0.0;
+ 
+	clock_t begin = clock();
  	printf("Hello, World!");
  	clock_t end = clock();
 	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
